a5b9824450808d6e07d14b8b14562fea684f7524
wanghui26 <wanghui26@xiaomi.com>
2023-04-29 17:33:36 +0800

    sort problems

--- src/com/hui/Top150/Arrays.java
+++ src/com/hui/Top150/Arrays.java
@@ -6,12 +6,15 @@ public class Arrays {
         int[] arr = new int[]{2, 3, 1, 1, 4};
         int res = jump2(arr);
         System.out.println(res);
+        System.out.println(jump(arr));
+        System.out.println(jump2_1(arr));
     }
 
     public static boolean jump(int[] nums) {
         int k = nums[0];
         for (int i = 1; i < nums.length; i++) {
-
+            if (i >= k) return true;
+            k = Math.max(k, nums[i] + i);
         }
         return false;
     }
@@ -29,5 +32,20 @@ public class Arrays {
             }
         }
         return steps;
+    }
+
+    public static int jump2_1(int[] nums) {
+        int step = 0;
+        int end = 0;
+        int maxPosition = 0;
+        int len = nums.length;
+        for (int i = 0; i < len - 1; i++) {
+            maxPosition = Math.max(maxPosition,i+nums[i]);
+            if(i == end){
+                end = maxPosition;
+                step++;
+            }
+        }
+        return step;
     }
 }
