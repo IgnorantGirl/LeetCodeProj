3744190566da113442a852ddc7357a7ce51092db
wanghui26 <wanghui26@xiaomi.com>
2023-05-04 10:31:46 +0800

    151　翻转字符串

--- src/com/hui/Top150/ReverseWords151.java
+++ src/com/hui/Top150/ReverseWords151.java
@@ -1,8 +1,14 @@
 package com.hui.Top150;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class ReverseWords151 {
     public static void main(String[] args) {
         System.out.println(reverseWords("  a  good   example  "));
+        System.out.println(reverseWords1("  a  good   example  "));
+
     }
 
     public static String reverseWords(String s) {
@@ -16,5 +22,12 @@ public class ReverseWords151 {
             }
         }
         return builder.toString();
+    }
+    public static String reverseWords1(String s) {
+         s = s.trim();
+        List<String> strList = Arrays.asList(s.split("\\s+"));
+        Collections.reverse(strList);
+        String res = String.join(" ",strList);
+        return res;
     }
 }
