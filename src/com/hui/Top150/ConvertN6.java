a1a04ef4a2afbb8bbc3a829bc20983ae6786e1de
wanghui26 <wanghui26@xiaomi.com>
2023-05-04 11:36:13 +0800

    6　Z变换

---
+++ src/com/hui/Top150/ConvertN6.java
@@ -0,0 +1,32 @@
+package com.hui.Top150;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertN6 {
+    public static void main(String[] args) {
+        //　正确结果　PAHNAPLSIIGYIR
+        System.out.println(convert("PAYPALISHIRING", 3));
+    }
+
+    public static String convert(String s, int numRows) {
+        // 变换行数的标识
+        int flag = -1;
+        StringBuilder res = new StringBuilder();
+        List<StringBuilder> rowList = new ArrayList<>();
+        //　向list里面添加　指定行数的StringBuilder,用于拼接每一行的string
+        for (int i = 0; i < numRows; i++) {
+            rowList.add(new StringBuilder());
+        }
+        for (int i = 0, n = 0; i < s.length(); i++, n += flag) {
+            rowList.get(n).append(s.charAt(i));
+            if (n == 0 || n == rowList.size()-1) {
+                flag = -flag;
+            }
+        }
+        for (StringBuilder builder : rowList) {
+            res.append(builder);
+        }
+        return res.toString();
+    }
+}
