439f1bf46e4fc4603f9ab976d973e55e937c6eec
wanghui26 <wanghui26@xiaomi.com>
2023-05-07 20:47:03 +0800

    209 长度最小的子数组

--- src/com/hui/Top150/SubArrayLen209.java
+++ src/com/hui/Top150/SubArrayLen209.java
@@ -3,13 +3,21 @@ package com.hui.Top150;
 public class SubArrayLen209 {
     public static void main(String[] args) {
         // [2,3,1,2,4,3]  2
+        System.out.println(minSubArrayLen(7,new int[]{2,3,1,2,4,3}));
     }
 
     public static int minSubArrayLen(int target, int[] nums) {
         int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-
+        // in　进入sum的元素；out 移出sum的元素
+        int in =0,out =0,res = Integer.MAX_VALUE;
+        while (in<nums.length){
+            sum = sum + nums[in];
+            while (sum>=target){
+                res = Math.min(res,in-out+1);
+                sum = sum-nums[out++];
+            }
+            in++;
         }
-        return sum;
+        return res == Integer.MAX_VALUE ? 0 : res;
     }
 }
