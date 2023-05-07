0b47003cfbe9e49f2a64750e91afd4063035f513
‘wanghui26’ <wanghui26@xiaomi.com>
2023-05-07 19:08:10 +0800

    ..

---
+++ src/com/hui/Top150/SubArrayLen209.java
@@ -0,0 +1,15 @@
+package com.hui.Top150;
+
+public class SubArrayLen209 {
+    public static void main(String[] args) {
+        // [2,3,1,2,4,3]  2
+    }
+
+    public static int minSubArrayLen(int target, int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+        return sum;
+    }
+}
